72289d355509463bd665c4e5202318f21df66bad
pradip <hazarikapradip133@gmail.com>
2024-06-18 21:53:30 +0500

    eslint fix

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,11 @@
-function login(username: string): boolean {
-    return true;
+function login(username: string): string {
+    // console.log(username);
+    const user = {
+        name: 'pradip',
+    };
+
+    const name = user.name;
+
+    return username + name;
 }
 login('pradip');
